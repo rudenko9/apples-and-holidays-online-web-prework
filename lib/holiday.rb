d0d2744428b26df8c2fbbcfc47a37f80095eb887
Nataliya R <n.rudenko@gmail.com>
2019-02-05 19:07:29 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,6 +17,9 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
+  
+  holiday_supplies[:summer][:fourth_of_july][1]
+  
   # return the second element in the 4th of July array
 end
 
@@ -24,6 +27,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  holiday_supplies[:winter][:christmas] << "#{supply}"
+  holiday_supplies[:winter][:new_years] << "#{supply}"
 
 end
 
@@ -31,17 +36,21 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << "#{supply}"
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  holiday_hash[season][holiday_name] = supply_array
+  holiday_hash
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+holiday_hash[:winter].values.flatten
 
 end
 
@@ -53,12 +62,26 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holiday|
+    puts season.to_s.capitalize! << ":"
+    holiday.each do |holiday, supplies|
+      supplies = supplies.join(", ")
+      holiday = holiday.to_s.split("_").map {|w| w.capitalize}.join(" ") << ":"
+      puts "  " + holiday + " " + supplies
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+holiday_hash.map do |season, holidays|
+  holidays.map do |holiday, items|
+    if items.include?("BBQ")
+      holiday
+    end
+  end
+end.flatten.compact
 
 end
 
